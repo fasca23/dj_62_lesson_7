8a42e03fcfb828257fc4b94e40c7ebaf2eb9bb04
Vladimir Kamenev <fasca22@gmail.com>
2023-02-21 17:32:16 +0300

    without authentication

--- advertisements/views.py
+++ advertisements/views.py
@@ -1,10 +1,13 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
+from advertisements.models import Advertisement
+from advertisements.serializers import AdvertisementSerializer
 
 
 class AdvertisementViewSet(ModelViewSet):
     """ViewSet для объявлений."""
-
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
     # TODO: настройте ViewSet, укажите атрибуты для кверисета,
     #   сериализаторов и фильтров
 
